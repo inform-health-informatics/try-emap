7ac42646ce62068257253354f50ac58f1992dbdd
Steve Harris <git@steveharris.me>
2020-05-10 00:28:16 +0100

    refactored so that can define start-stop for any column or time

--- vignettes/find_critical_care_admissions.R
+++ vignettes/find_critical_care_admissions.R
@@ -1,6 +1,15 @@
 # Steve Harris
-# 2020-05-06
+# Created 2020-05-06
+# Change log
+# 2020-05-09
+# - work to define department at the functional rather than the ward level
+# i.e. P03 and T03 are both critical care
+# - plot LoS over time using R plotly
+
+
+
 # Calculate the severity of illness models for critical care
+
 # Part 1: Identify critical care admissions
 # Part 2: define the outcome
 # Part 3: define the admitting physiology
@@ -47,68 +56,110 @@ ctn <- DBI::dbConnect(RPostgres::Postgres(),
 # You can load these as follows if you wish e.g.
 # query <- read_file("snippets/SQL/bed_moves.sql")
 
+
+column_jump <- function(dt, col, order_vars, group, col_jump) {
+        # helper function for collapse_over
+        # Input: a data.table
+        # Output: a data.table with a column (col) that flags 'steps'
+        
+        # define updates where the location changes; then keep those and the NAs
+        # checks to see if the department has changed compared to the prev entry
+        
+        dt[order(get(order_vars)), 
+             (col_jump) := shift(.SD, type="lag"), 
+             by=list(get(group)), 
+             .SDcols=col][
+             , (col_jump) :=  get(col_jump) != get(col)]
+        return(dt)
+        
+}
+
+time_jump <- function(dt, in_time, out_time, order_vars, group) {
+        # helper function for collapse_over
+        # Input: a data.table with cols for in and out times (e.g. admission and discharge)
+        # Output: a data.table with a column that flags 'steps' in a time-series
+        
+        # now need logic to check whether the gap between times is too big (i.e. a re-admission or not)
+        # Specify gap between times for a new location to be considered a 're-admission'
+        col_jump <- 'out2in_jump'
+        
+        dt[order(get(order_vars)), 
+             (col_jump) := shift(.SD, type="lag"), 
+             by=list(get(group)), 
+             .SDcols=out_time][
+             , (col_jump) :=  get(in_time) - get(col_jump)]
+        return(dt)
+        
+}
+
+
+collapse_over <- function(dt, col, in_time, out_time, order_vars, group, time_jump_window=dhours(4)) {
+        # defines ward transitions based on a time series in of ward names
+        # and a gap of sufficent size in the time that a re-admission is likely
+        tdt <- data.table::copy(dt)
+        col_jump <- paste0(col, '_jump')
+        
+        tdt <- column_jump(tdt, col, order_vars, group, col_jump)
+        tdt <- time_jump(tdt, in_time, out_time, order_vars, group)
+        
+        # Now to collapse over ward episodes, just keep where either ward_jump | time_jump is true
+        # keep all time_jumps > a window
+        temp <- tdt[get(col_jump) | abs(out2in_jump) > time_jump_window | is.na(get(col_jump)) | is.na(out2in_jump)]
+        # now number the department moves: department_i
+        col_i <- paste0(col, '_i')
+        temp[order(get(order_vars)), (col_i) := seq_len(.N), by=.(get(group))][]
+        
+        # finally join this back on to dt
+        tdt <- temp[tdt, on=.NATURAL]
+        
+        # then roll the department variable forwards and back
+        tdt[order(get(order_vars)), (col_i) := nafill(get(col_i), type='locf'), by=.(get(group))][]
+        
+        # drop intermediate columns
+        tdt[, (c(col_jump, "out2in_jump")) := NULL] # drop these intermediate columns
+        
+        # department admission
+        col_in <- paste0(col,'_',in_time)
+        tdt[order(get(order_vars)), (col_in) := min(get(in_time)), by=.(get(group),get(col_i)) ]
+        
+        # department discharge
+        col_out <- paste0(col,'_',out_time)
+        tdt[order(get(order_vars)), (col_out) := max(get(out_time)), by=.(get(group),get(col_i)) ]
+        
+        return(tdt)
+}
+
 # Load bed moves
 # ==============
 # THis query returns approx 1e6 rows so it takes a minute or so
 query <- "SELECT * FROM uds.star.bed_moves"
-dt <- DBI::dbGetQuery(ctn, query)
-setDT(dt)
-
+rdt <- DBI::dbGetQuery(ctn, query)
+setDT(rdt)
+wdt <- data.table::copy(rdt)
 
 # The line below is a quick trick to inspect all departments
 # View(dt[,.N,by=department][order(-N)])
+
 # First find all MRNs that have been to a critical care area
-mrncc <- unique(dt[department == "UCH T03 INTENSIVE CARE" |
-            department == "UCH P03 CV" |
-             department == "UCH T07 HDRU"][,.(mrn)])
+# Narrow definition of critical care
+critical_care_departments <- c( "UCH T03 INTENSIVE CARE", "UCH P03 CV" )
+# Broad definition of critical care
+# critical_care_departments <- c( "UCH T03 INTENSIVE CARE", "UCH P03 CV" , "UCH T07 HDRU")
+
+wdt[, critcare := department %in% critical_care_departments]
+mrncc <- unique(wdt[critcare == TRUE][,.(mrn)])
+
 # then select all bedmoves related to those patients
-dtcc <- dt[mrncc, on="mrn"][order(mrn,admission)]
-head(dtcc)
+wdt <- wdt[mrncc, on="mrn"][order(mrn,admission)]
+head(wdt)
 
-# Now collapse by department to appropriately define ICU admission
-# dtcc <- dtcc[,.(mrn,admission,discharge,department)]
+# Now collapse by department to appropriately define department level moves
+tdt <- collapse_over(wdt, col='department', in_time='admission', out_time='discharge', order_vars=c('mrn','admission'), group='mrn')
+head(tdt)
+# Now collapse by 'critcare' to appropriately define critical care admission periods
+tdt <- collapse_over(tdt, col='critcare', in_time='admission', out_time='discharge', order_vars=c('mrn','admission'), group='mrn')
+head(tdt)
 
-# define updates where the location changes; then keep those and the NAs
-# checks to see if the department has changed compared to the prev entry
-dtcc[order(mrn,admission), 
-     department_jump := shift(.SD, type="lag"), 
-     by=mrn, 
-     .SDcols="department"][
-     , department_jump :=  department_jump != department][]
-
-# now need logic to check whether the gap between times is too big (i.e. a re-admission or not)
-# Specify gap between times for a new location to be considered a 're-admission'
-
-dtcc[order(mrn,admission),  
-     time_jump := shift(.SD, type="lag"),  
-     by=mrn,  .SDcols="discharge"][,
-           time_jump := (admission-time_jump)]
-# View(dtcc[unique(dtcc[time_jump>0][,.(mrn)]), on="mrn"])
-
-# Now to collapse over ward episodes, just keep where either ward_jump | time_jump is true
-# keep all time_jumps > a window
-time_jump_window  <- dhours(4)
-temp <- dtcc[department_jump | abs(time_jump) > time_jump_window | is.na(department_jump) | is.na(time_jump)]
-# now number the department moves: department_i
-temp[order(mrn,admission), department_i := seq_len(.N), by=.(mrn)][]
-# # then create an reversed version (department_r) that you can use to identify the last move
-# temp[order(mrn,-admission), department_r := seq_len(.N), by=.(mrn)][]
-
-# finally join this back on to dtcc
-# dtcc[, c("department_i", "department_r") := NULL]
-dtcc <- temp[dtcc, on=.NATURAL]
-
-# then roll the department variable forwards and back
-dtcc[order(mrn,admission), department_i := nafill(department_i, type='locf'), by=mrn][]
-# dtcc[order(mrn,admission), department_r := nafill(department_r, type='locf'), by=mrn][]
-
-# drop intermediate columns
-dtcc[, (c("department_jump", "time_jump", "hl7_location")) := NULL] # drop these intermediate columns
-
-# department admission
-dtcc[order(mrn,admission), dpt_admit_dt := min(admission), by=.(mrn,department_i) ]
-# department discharge
-dtcc[order(mrn,admission), dpt_disch_dt := max(discharge), by=.(mrn,department_i) ]
 
 fwrite(dtcc, file="data/secure/critical_care_bed_moves.csv")
 
